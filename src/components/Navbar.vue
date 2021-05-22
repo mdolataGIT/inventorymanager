a0f74d48c54e9376d59363f6c05d4a405612ec38
mdolataGIT <maciejdolata69@gmail.com>
2021-05-22 18:40:42 +0200

    Authorization

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,35 @@
     <div class="nav-wrapper green">
       <div class="container">
         <router-link to="/" class="brand-logo">Inventory Manager</router-link>
+        <ul class="right">
+          <li><router-link to="/">Dashboard</router-link></li>
+          <li><router-link to="/login">Login</router-link></li>
+          <li><router-link to="/register">Register</router-link></li>
+          <li><button v-on:click="logout" class="btn black">Logout</button></li>
+        </ul>
       </div>
     </div>
   </nav>
 </template>
+<script>
+import firebase from "firebase";
+export default {
+  name: "navbar",
+  data() {
+    return {
+      isLoggedIn: false,
+      currentUser: false,
+    };
+  },
+  methods: {
+    logout: function () {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.$router.push("/login");
+        });
+    },
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -8,51 +8,125 @@ import ViewInventory from '@/components/ViewInventory'
 import EditInventory2 from '@/components/EditInventory2'
 import ViewInventory2 from '@/components/ViewInventory2'
 import Dashboard2 from '@/components/Dashboard2'
+import Login from '@/components/Login'
+import Register from '@/components/Register'
+import firebase from 'firebase'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
     {
       path: '/',
       name: 'dashboard',
-      component: Dashboard
+      component: Dashboard,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: {
+        requiresGuest: true
+      }
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+      meta: {
+        requiresGuest: true
+      }
     },
     {
       path: '/dashboard2/:garden_id',
       name: 'dashboard2',
-      component: Dashboard2
+      component: Dashboard2,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/new',
       name: 'new-inventory',
-      component: NewInventory
+      component: NewInventory,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/new2/:garden_id',
       name: 'new-inventory2',
-      component: NewInventory2
+      component: NewInventory2,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/edit/:garden_id',
       name: 'edit-inventory',
-      component: EditInventory
+      component: EditInventory,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/edit2/:inventory_id',
       name: 'edit-inventory2',
-      component: EditInventory2
+      component: EditInventory2,
+      meta: {
+        requiresAuth: true,
+      }
     },
     {
       path: '/:inventory_id',
       name: 'view-inventory2',
-      component: ViewInventory2
+      component: ViewInventory2,
+      meta: {
+        requiresAuth: true
+      }
     },
 
     {
       path: '/:garden_id',
       name: 'view-inventory',
-      component: ViewInventory
+      component: ViewInventory,
+      meta: {
+        requiresAuth: true
+      }
     },
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!firebase.auth().currentUser) {
+      next({
+        path: '/login',
+        query: {
+          redirect: to.fullPath
+        }
+      });
+    } else {
+      next();
+    }
+  } else if (to.matched.some(record => record.meta.requiresGuests)) {
+    if (!firebase.auth().currentUser) {
+      next({
+        path: '/',
+        query: {
+          redirect: to.fullPath
+        }
+      });
+    } else {
+      next();
+    }
+
+  } else {
+    next();
+  }
+})
+
+export default router;
